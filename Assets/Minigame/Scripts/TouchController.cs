8ddcc9920763103e23a09ffc6b2350236b8a3fb3
Shusuke <oishiinoumiso@gmail.com>
2018-08-16 15:59:30 +0900

    Introduction Function MultiTouch

--- Assets/Minigame/Scripts/TouchController.cs
+++ Assets/Minigame/Scripts/TouchController.cs
@@ -39,22 +39,21 @@ public class TouchController : MonoBehaviour
 
         BulletInterval += Time.deltaTime;
 
-        //Rayの作成
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
         //Rayが当たったオブジェクトの情報を入れる箱
         RaycastHit hit;
 
         //Rayの飛ばせる距離
         int distance = 100;
 
-        //Rayの可視化    ↓Rayの原点　　　　↓Rayの方向　　　　　　　　　↓Rayの色
-        Debug.DrawLine(ray.origin, ray.direction * distance, Color.red);
-
+        #if UNITY_EDITOR
         //もしRayにオブジェクトが衝突したら
         //                  ↓Ray  ↓Rayが当たったオブジェクト ↓距離
         if (Input.GetMouseButton(0))
         {
+            //Rayの作成
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //Rayの可視化    ↓Rayの原点　　　　↓Rayの方向　　　　　　　　　↓Rayの色
+            Debug.DrawLine(ray.origin, ray.direction * distance, Color.red);
             // 処理
             //                  ↓Ray  ↓Rayが当たったオブジェクト ↓距離
             if (Physics.Raycast(ray, out hit, distance))
@@ -71,7 +70,7 @@ public class TouchController : MonoBehaviour
                 //Rayが当たったオブジェクトのtagがShootだったら
                 if (hit.collider.tag == "Shoot")
                 {
-                    if (BulletInterval >= 0.8f)
+                    if (BulletInterval >= 1.5f)
                     {
                         BulletShoot();
                     }
@@ -90,6 +89,54 @@ public class TouchController : MonoBehaviour
                 }
             }
         }
+#endif
+
+#if UNITY_ANDROID
+        if (Input.touchCount > 0)
+        {
+            Touch[] myTouches = Input.touches;
+            for (int i = 0; i < myTouches.Length; i++)
+            {
+                //Rayの作成
+                ray = Camera.main.ScreenPointToRay(myTouches[i].position);
+                //Rayの可視化    ↓Rayの原点　　　　↓Rayの方向　　　　　　　　　↓Rayの色
+                Debug.DrawLine(ray.origin, ray.direction * distance, Color.yellow);
+
+                if (Physics.Raycast(ray, out hit, distance))
+                {
+                    Debug.Log(hit.collider.tag);
+                    //Rayが当たったオブジェクトのtagがGameScreenだったら
+                    if (hit.collider.tag == "GameScreen")
+                    {
+                        ObjectManager.GameStart();
+                    }
+
+                    BulletInterval += Time.deltaTime;
+
+                    //Rayが当たったオブジェクトのtagがShootだったら
+                    if (hit.collider.tag == "Shoot")
+                    {
+                        if (BulletInterval >= 1.5f)
+                        {
+                            BulletShoot();
+                        }
+                    }
+
+                    //Rayが当たったオブジェクトのtagがLeftだったら
+                    if (hit.collider.tag == "Left")
+                    {
+                        MoveToLeft(horizontal);
+                    }
+
+                    ////Rayが当たったオブジェクトのtagがRightだったら
+                    if (hit.collider.tag == "Right")
+                    {
+                        MoveToRight(horizontal);
+                    }
+                }
+            }
+        }
+        #endif
     }
 
    void BulletShoot(){
